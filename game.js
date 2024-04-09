b0555c8c49463c035738b8b3e7869764778ded0a
Kastor Kitov <kitov.kastor@gmail.com>
2024-04-09 11:11:16 +0300

    Ball 2D Motion,Paddle

--- game.js
+++ game.js
@@ -1,7 +1,25 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
+var ballY = 50;
 var ballSpeedX = 10;
+var ballSpeedY = 4;
+
+var paddle1Y = 250;
+const PADDLE_HEIGHT = 100;
+
+function calculateMousePosition(evt){
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+
+    return{
+        x:mouseX,
+        y:mouseY
+    }
+}
+
 window.onload = function(){
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
@@ -11,22 +29,36 @@ window.onload = function(){
         moveEverything();
         drawEverything();
     }, 1000/framesPerSecound);
+
+    canvas.addEventListener('mousemove',function (evt){
+        var mousePos = calculateMousePosition(evt);
+        paddle1Y = mousePos.y - (PADDLE_HEIGHT/2);
+    })
 }
 function moveEverything(){
+    //move left and right
     ballX = ballX + ballSpeedX;
     if(ballX >= canvas.width){
         ballSpeedX = -ballSpeedX;
     }
-
     if(ballX < 0){
         ballSpeedX = -ballSpeedX;
+    }
+
+    //move up and down
+    ballY = ballY + ballSpeedY;
+    if(ballY >= canvas.width){
+        ballSpeedY = -ballSpeedY;
+    }
+    if(ballY < 0){
+        ballSpeedY = -ballSpeedY;
     }
 }
 function drawEverything() {
 
     colorRect(0,0,canvas.width,canvas.height, 'black');
-    colorRect(0,210,10,100,'white');
-    colorCircle(ballX, 150, 10, 'white')
+    colorRect(0,paddle1Y,10,PADDLE_HEIGHT,'white');
+    colorCircle(ballX, ballY, 10, 'white')
 }
 
 function colorRect(leftX, topX, width, height, drawColor){
